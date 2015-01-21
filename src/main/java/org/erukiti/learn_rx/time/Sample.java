f3397e7c55e19e55c26597508b9eb3703b52d38b
erukiti <erukiti@gmail.com>
2015-01-21 10:11:57 +0900

    RxJava: 時間系のオペレータの追加・変更
    * throttleFirst, throttleLast のストリームサンプルが割とタイミング依存なせいで書けない点がどうしようもない気がしてきた

--- src/main/java/org/erukiti/learn_rx/time/Sample.java
+++ src/main/java/org/erukiti/learn_rx/time/Sample.java
@@ -3,19 +3,22 @@ package org.erukiti.learn_rx.time;
 import rx.Observable;
 import java.util.concurrent.TimeUnit;
 
-public class ThrottleFirst {
+public class Sample {
     public static void main(String[] args) {
+        /*
+         * stream: -a--b-c--d--e-|
+         *          v   |v   |
+         * stream: -a----c---|
+         */
         Observable
                 .just(100, 200, 100, 200, 200)
                 .concatMap(ms -> Observable.just(ms).delay(ms, TimeUnit.MILLISECONDS))
-                .throttleFirst(350, TimeUnit.MILLISECONDS)
+                .sample(Observable.interval(350, TimeUnit.MILLISECONDS))
                 .timeInterval()
                 .toBlocking()
                 .forEach(n -> System.out.println(n));
 
         System.out.println("****");
-        // FIXME: なんだかタイミング調整が難しい
-
     }
 
 }
--- src/main/java/org/erukiti/learn_rx/time/ThrottleLast.java
+++ src/main/java/org/erukiti/learn_rx/time/ThrottleLast.java
@@ -5,23 +5,15 @@ import java.util.concurrent.TimeUnit;
 
 public class ThrottleLast {
     public static void main(String[] args) {
-        /*
-         * stream: -a--b-c---d-e-|
-         *          v   |v    |v
-         * stream: -a----c-----e-|
-         */
         Observable
-                .just(100, 200, 100, 300, 100)
+                .just(100, 200, 100, 200, 200)
                 .concatMap(ms -> Observable.just(ms).delay(ms, TimeUnit.MILLISECONDS))
-                .throttleLast(350, TimeUnit.MILLISECONDS)
+                .throttleFirst(350, TimeUnit.MILLISECONDS)
                 .timeInterval()
                 .toBlocking()
                 .forEach(n -> System.out.println(n));
 
         System.out.println("****");
-        // FIXME: タイミング調整が不十分
-
-        // FIXME: 複数 obs 使った sample
     }
-
+    // FIXME: なんだかタイミング調整が難しい
 }
